e14c1ca1497c5bb901522c44f1e67c4c3e75b295
dtw <dtw@users.noreply.github.com>
2019-06-20 17:18:10 +0100

    Set default Page sort in admin to most recent date

--- functions.php
+++ functions.php
@@ -38,18 +38,19 @@ if ( $query->is_tax('signpost_categories') ) {
 }
 add_action( 'pre_get_posts', 'hwb_pre_get_posts' );
 
+// Sort pages in backend by most recent
+function set_post_order_in_admin( $wp_query ) {
 
+global $pagenow;
 
+if ( is_admin() && 'edit.php' == $pagenow && !isset($_GET['orderby'])) {
 
+    $wp_query->set( 'orderby', 'date' );
+    $wp_query->set( 'order', 'DESC' );
+}
+}
 
-
-
-
-
-
-
-
-
+add_filter('pre_get_posts', 'set_post_order_in_admin', 5 );
 
 
 
