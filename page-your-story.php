f48652b529e3f4f4aa854ab6cd3917f7c574067f
dtw <dtw@users.noreply.github.com>
2019-08-07 13:23:03 +0100

    Update message_text field

--- page-your-story.php
+++ page-your-story.php
@@ -95,6 +95,11 @@
               <div id="respond">
                 <?php echo $response; ?>
                 <form action="<?php the_permalink(); ?>" method="post">
+                  <p class="comment-form-comment">
+                    <label for="comment">What happened?</label>
+                    <p>Please tell us what happened and make suggestions for improvements. Please do not include any personal information like names, dates or detailed health information.</p>
+                    <textarea required tabindex="1" id="comment" name="message_text" type="text" cols="45" rows="8" aria-required="true"><?php echo esc_textarea($_POST['message_text']); ?></textarea>
+                  </p>
                   <p class="comment-form-author">
                     <label for="message_name">Your name</label>
                     <input placeholder="Your first and last names (optional)" id="author" name="message_name" type="text" size="30" tabindex="6" value="<?php echo esc_attr($_POST['message_name']); ?>"/>
@@ -109,8 +114,6 @@
                   </p>
                   <h2>Privacy</h2>
                   <p>Please review our <a href="https://www.healthwatchbucks.co.uk/data-protection-privacy-policy/" target="_blank">data protection policy</a>. By completing this form, you agree that you have read and understood the privacy information provided, and confirm you are over 18.</p>
-
-                  <p><label for="message_text">Message: <span>*</span> <br><textarea type="text" name="message_text"><?php echo esc_textarea($_POST['message_text']); ?></textarea></label></p>
                   <p><label for="message_human">Human Verification: <span>*</span> <br><input type="text" style="width: 60px;" name="message_human"> + 3 = 5</label></p>
                   <input type="hidden" name="submitted" value="1">
                   <p><input type="submit"></p>
