92ed629b62c1605725a7606c90b0a8403a06dbcd
dtw <dtw@users.github.com>
2019-10-24 12:13:07 +0100

    Restore sidebar lost in merge

--- functions.php
+++ functions.php
@@ -161,4 +161,14 @@ if ( function_exists('register_sidebar') )
   )
 );
 
+if ( function_exists('register_sidebar') )
+  register_sidebar(array(
+    'name' => 'Your Feedback Page',
+    'before_widget' => '<div class="row">',
+    'after_widget' => '</div>',
+    'before_title' => '<h3>',
+    'after_title' => '</h3>',
+  )
+);
+
 ?>
