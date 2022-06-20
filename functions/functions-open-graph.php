f5ef4dad75d230ae3a06b405465e4ea69aee9be1
Phil T <362017+dtw@users.noreply.github.com>
2022-06-20 09:07:46 +0100

    Use twitter handle from theme customiser

--- functions/functions-open-graph.php
+++ functions/functions-open-graph.php
@@ -34,11 +34,12 @@ function insert_fb_in_head() {
 // Twitter
 		echo '<!-- scaffold meta for Twitter -->';
 		echo '<meta name="twitter:card" content="summary_large_image" />';
-		echo '<meta name="twitter:creator" content="@HW_Bucks" />';
+		echo '<meta name="twitter:creator" content="@' . get_theme_mod( 'scaffold_org_twitter') . '" />';
 		echo '<meta name="twitter:title" content="' . get_the_title() . '" />';
 		echo '<meta name="twitter:description" content="' . get_the_excerpt() . '" />';
 		echo '<meta name="twitter:image:alt" content="' . get_the_title() . '" />';
 	if(!has_post_thumbnail( $post->ID )) { //the post does not have featured image, use a default image
+		// $default_image= get_site_icon_url(); //replace this with a default image on your server or an image in your media library
 		$default_image="http://www.healthwatchbucks.co.uk/wp-content/uploads/2016/07/Patterned-Quotes.png"; //replace this with a default image on your server or an image in your media library
 		echo '<meta property="twitter:image" content="' . $default_image . '"/>';
 	}
