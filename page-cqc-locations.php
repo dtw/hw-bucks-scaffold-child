43a926244ca8a73f0ebaff1d7154fa6a38507465
Phil T <362017+dtw@users.noreply.github.com>
2022-07-07 10:40:30 +0100

    Switch how services are matched
    Previously, we looped through each API location, then through every local_service to find matches.
    
    Instead, we clean up the API locations to remove those not registered. Then we loop through every local_service to find matches in the API locations and remove them.
    
    We end up with just the API locations that are not in the dbase.

--- page-cqc-locations.php
+++ page-cqc-locations.php
@@ -10,14 +10,31 @@ $api_response = json_decode(hw_feedback_cqc_api_query_locations(array(
       'partnerCode' => 'HW_BUCKS'
     )));
 
-echo '<h1>CQC Locations</h1>';
-echo '<p>API Query: <a href="https://api.cqc.org.uk/public/v1' . $api_response->firstPageUri . '" target="_blank">https://api.cqc.org.uk/public/v1' . $api_response->firstPageUri . '</a>)</p>';
-echo '<h2>Matched</h2>';
+// Convert "JSON object" to array
+$locations = array_values($api_response->locations);
+
 // get the total count of results
 $total = $api_response->total;
 $registered_counter = 0;
 $matched_count = 0;
 
+// Clean out anything not Registered
+foreach ($locations as $key => $current_location) {
+  $current_location_id = $current_location->locationId;
+  // get the location details
+  $current_location_detail = json_decode(hw_feedback_cqc_api_query_by_id('locations',$current_location->locationId));
+  if ($current_location_detail->registrationStatus != 'Registered') {
+    //echo '<p>'. $current_location->locationName . ' (<a href="https://www.cqc.org.uk/location/' . $current_location->locationId . '" target="_blank">' . $current_location->locationId . '</a>)</p>';
+    unset($locations[$key]);
+    continue;
+  }
+  $registered_counter++;
+}
+// Reindex array - THIS IS CRITICAL!
+$locations = array_values($locations);
+
+echo '<h1>CQC Locations</h1>';
+echo '<p>API Query: <a href="https://api.cqc.org.uk/public/v1' . $api_response->firstPageUri . '" target="_blank">https://api.cqc.org.uk/public/v1' . $api_response->firstPageUri . '</a>)</p>';
 $my_query = new WP_Query( array(
   'posts_per_page' => -1,
   'post_type' => 'local_services',
@@ -26,46 +43,37 @@ $my_query = new WP_Query( array(
   )
 );
 
-$locations = array_values($api_response->locations);
-foreach ($locations as $key => $current_location) {
-  $current_location_id = $current_location->locationId;
-  // get the location details
-  $current_location_detail = json_decode(hw_feedback_cqc_api_query_by_id('locations',$current_location_id));
-  // get the reg status
-  $current_location_status = $current_location_detail->registrationStatus;
-  $current_location_type = $current_location_detail->inspectionDirectorate;
-
-  if ($current_location_status == 'Registered') {
-    $registered_counter++;
-    // otherwise
-    $current_location_name = $current_location_detail->name;
-    while ($my_query->have_posts()) {
-      $my_query->the_post();
-      $our_location_id = get_post_meta( $post->ID, 'hw_services_cqc_location', true );
-      if ($current_location_id == $our_location_id) {
-        echo '<p>'. $current_location_name . ' (<a href="https://www.cqc.org.uk/location/' . $current_location_id . '" target="_blank">' . $current_location_id . '</a>)</p>';
-        $matched_count ++;
-        // this really is a thing - if we just break the loop, we stay at the same point in the query results, so we need to rollback to the start of the query
-        $my_query->rewind_posts();
-        unset($locations[$key]);
-        break 1;
-      }
-    }
-  } else {
-    // it's not Registered
-    unset($locations[$key]);
+echo '<h2>Matched in hw-feedback</h2>';
+// loop the posts
+while ($my_query->have_posts()) {
+  $my_query->the_post();
+  // get the CQC Location ID from post_meta
+  $our_location_id = get_post_meta( $post->ID, 'hw_services_cqc_location', true );
+  // search for the location_id in $locations
+  $result = array_search($our_location_id, array_column($locations, 'locationId'));
+  // $result can return empty which PHP can read as [0] - so check it is not empty
+  if ($result !== false){
+    $current_location_name = $locations[$result]->locationName;
+    $current_location_id = $locations[$result]->locationId;
+    echo '<p>'. $current_location_name . ' (<a href="https://www.cqc.org.uk/location/' . $current_location_id . '" target="_blank">' . $current_location_id . '</a>)</p>';
+    // count the match
+    $matched_count ++;
+    // remove the service from $locations
+    unset($locations[$result]);
   }
 }
 echo '<p>Matched: ' . $matched_count . '/' . $registered_counter . '</p>';
-echo '<h2>Un-matched</h2>';
+// Reindex array - THIS IS CRITICAL!
+$locations = array_values($locations);
 
-//$unmatched_locations = $api_response->locations;
+echo '<h2>Un-matched / To be added</h2>';
+// loop the remaning $locations
 foreach ($locations as $location) {
   echo '<p>'. $location->locationName . ' (<a href="https://www.cqc.org.uk/location/' . $location->locationId . '" target="_blank">' . $location->locationId . '</a>)</p>';
 }
-
+echo '<p>Un-matched: ' . count($locations) . '/' . $registered_counter . '</p>';
+// Get finish time
 $executionEndTime = microtime(true);
-
 // The result will be in seconds and milliseconds.
 $seconds = $executionEndTime - $executionStartTime;
 // Print it out
