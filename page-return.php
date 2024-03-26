d4a6c11b674f246ef3895ac86eece6e2ae0486b5
Phil T <362017+dtw@users.noreply.github.com>
2024-03-26 15:45:54 +0000

    Whitespace

--- page-return.php
+++ page-return.php
@@ -1,17 +1,15 @@
 <?
-	// check for src in the URL
-	if ( isset($_GET['src']) ) {
-		$src = $_GET['src'];
-		// delete the cookie when we come back from SM
-		unset($_COOKIE['comment_id']);
-    // set a cookie with the src
-    setcookie('src', $src, time()+HOUR_IN_SECONDS, COOKIEPATH, COOKIE_DOMAIN);
-    nocache_headers();
-		$location = $the_url . '/thanks-' . $src . '/';
-    wp_safe_redirect( $location, 303 );
-    exit;
-	} else {
-		echo "No src detected.";
-	}
-
-?>
+// check for src in the URL
+if (isset($_GET['src'])) {
+  $src = $_GET['src'];
+  // delete the cookie when we come back from SM
+  unset($_COOKIE['comment_id']);
+  // set a cookie with the src
+  setcookie('src', $src, time() + HOUR_IN_SECONDS, COOKIEPATH, COOKIE_DOMAIN);
+  nocache_headers();
+  $location = $the_url . '/thanks-' . $src . '/';
+  wp_safe_redirect($location, 303);
+  exit;
+} else {
+  echo "No src detected.";
+}
