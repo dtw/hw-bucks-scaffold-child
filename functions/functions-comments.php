8c8b439819c508826501a2f47c27bf5f38c956b1
Phil T <362017+dtw@users.noreply.github.com>
2024-03-26 15:55:21 +0000

    Whitespace

--- functions/functions-comments.php
+++ functions/functions-comments.php
@@ -2,18 +2,17 @@
 
 // Redirect to thank you post after comment on local_services
 
-add_action('comment_post_redirect', 'redirect_to_thank_page', 10,2 );
+add_action('comment_post_redirect', 'redirect_to_thank_page', 10, 2);
 
-function redirect_to_thank_page( $location, $commentdata) {
-	$the_url = site_url();
+function redirect_to_thank_page($location, $commentdata)
+{
+  $the_url = site_url();
   $post_id = $commentdata->comment_post_ID;
-	$cid = $commentdata->comment_ID;
-  if('local_services' == get_post_type($post_id)){
-		// set a cookie with the comment id in it - we need this on the next page
-		setcookie('comment_id', $cid, time()+HOUR_IN_SECONDS, COOKIEPATH, COOKIE_DOMAIN);
-		return $the_url . '/about-you/';
+  $cid = $commentdata->comment_ID;
+  if ('local_services' == get_post_type($post_id)) {
+    // set a cookie with the comment id in it - we need this on the next page
+    setcookie('comment_id', $cid, time() + HOUR_IN_SECONDS, COOKIEPATH, COOKIE_DOMAIN);
+    return $the_url . '/about-you/';
   }
   return $location;
-	}
-
-?>
+}
