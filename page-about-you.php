bf1283f668c41fe096701c285380e94d196c5fe0
Phil T <362017+dtw@users.noreply.github.com>
2024-03-26 15:47:48 +0000

    Whitespace

--- page-about-you.php
+++ page-about-you.php
@@ -1,13 +1,13 @@
 <?php get_header(); ?>
-  <div class="container">
-    <!--<div id="content" role="main" class="col-md-8 col-xs-12">-->
-    <div class="row">
-      <div id="content" class="col-md-8 col-xs-12">
-        <div class="alert alert-success alert-relative" role="alert">
-          Success - your comment has been sent!
-        </div>
-        <div id="referral-buttons" class="row">
-        <?php if ( isset($_COOKIE['comment_id']) ) {
+<div class="container">
+  <!--<div id="content" role="main" class="col-md-8 col-xs-12">-->
+  <div class="row">
+    <div id="content" class="col-md-8 col-xs-12">
+      <div class="alert alert-success alert-relative" role="alert">
+        Success - your comment has been sent!
+      </div>
+      <div id="referral-buttons" class="row">
+        <?php if (isset($_COOKIE['comment_id'])) {
           // read the cookie we set when the comment form was submitted
           $comment_id = $_COOKIE['comment_id'];
           // now remove the cookie
@@ -22,31 +22,31 @@
             <div class="col-sm-9 col-xs-12">
               <p>By telling us more about yourself, you can help us address poor health outcomes across Buckinghamshire.</p>
               <div id="yes-button">
-                <?php echo '<a class="btn btn-cta" href="https://www.smartsurvey.co.uk/s/HW_BUCKS_DEMO/?src=website&id=' . $comment_id . '">Contribute your data</a>';?>
+                <?php echo '<a class="btn btn-cta" href="https://www.smartsurvey.co.uk/s/HW_BUCKS_DEMO/?src=website&id=' . $comment_id . '">Contribute your data</a>'; ?>
               </div>
               <!--<div id="no-button" class="row">
                 <a href="https://www.healthwatchbucks.co.uk/thanks-website/">No, I don't want to answer these questions</a>
               </div> -->
             </div>
           </div>
-          <?php } ?>
-        </div>
-        <?php while ( have_posts() ) : the_post(); ?>
+        <?php } ?>
+      </div>
+      <?php while (have_posts()) : the_post(); ?>
 
-            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-              <div class="entry-content">
-                <?php the_content(); ?>
-              </div>
-            </article><!-- #post -->
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+          <div class="entry-content">
+            <?php the_content(); ?>
+          </div>
+        </article><!-- #post -->
 
         <?php endwhile; // end of the loop. ?>
-      </div> <!-- #content -->
-      <div id="sidebar" class="col-md-4 hidden-sm hidden-xs">
-        <div id="clipboard-cropped"></div>
-      </div>
-    </div><!-- .row -->
-    <!-- Your Feedback Page Widget Area-->
-    <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("About You Page") ) : ?>
-    <?php endif;?>
-  </div><!-- .container -->
+    </div> <!-- #content -->
+    <div id="sidebar" class="col-md-4 hidden-sm hidden-xs">
+      <div id="clipboard-cropped"></div>
+    </div>
+  </div><!-- .row -->
+  <!-- Your Feedback Page Widget Area-->
+  <?php if (!function_exists('dynamic_sidebar') || !dynamic_sidebar("About You Page")) : ?>
+  <?php endif; ?>
+</div><!-- .container -->
 <?php get_footer(); ?>
